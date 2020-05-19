54cb9c6e8644cb8f754be0d7feabf8c74bc04d77
sstefanovich <steve@digitalfringe.com>
2020-05-18 20:03:16 -0500

    Add JSONP Settings Loading

---
+++ RDA.Shopify.ScriptTag/Models/ShopSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDA.Shopify.ScriptTag.Models
+{
+    public class ShopSettings
+    {
+        public string ShopName { get; set; }
+
+        public string WelcomeMessage { get; set; }
+    }
+}
